bf7a3b48cc9545108ffb2333bba43204bb0c59c5
josh-Muleshi <jmuleshi2@gmail.com>
2022-04-06 20:32:28 +0200

    fisrt commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+    /**
+     * @package myspace
+     * 
+     * MySpace est une application de gestion des profils 
+     * spetialement pour les etudiants d'esis
+     * 
+     * @author Josh Muleshi <jmuleshi2@gmail.com>
+     */
+
+     echo "MySpace";
