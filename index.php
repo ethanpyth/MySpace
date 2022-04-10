bbcd14885b3faae0af04f2847bfc02f6b38822a5
Josh <60498337+josh-Muleshi@users.noreply.github.com>
2022-04-10 23:01:05 +0200

    MYS-1 new commit

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
      * @package myspace
      * 
      * MySpace est une application de gestion des profils 
-     * specialement pour les etudiants d'esis
+     * specialement pour les etudiants d'esis test
      * 
      * @author Josh Muleshi <jmuleshi2@gmail.com>
      */
