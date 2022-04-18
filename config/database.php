9058be4f8dd07be3f2bd6f11433ac6307b8b3057
Excellence KAWEJ <19ka201@esisalama.org>
2022-04-18 23:23:17 +0200

    reconfigure the database.php

--- config/database.php
+++ config/database.php
@@ -4,8 +4,12 @@
  * Php file to get databse instance
  */
 
+require '../vendor/autoload.php';
+
 use kreait\Firebase\Factory;
 
-$factory = (new Factory)->withServiceAccount($_ENV['PATH_MYSPACE_JSON']);
+$factory = (new Factory)
+    ->withServiceAccount($_ENV['PATH_MYSPACE_JSON'])
+    ->withDatabaseUri('https://myspace-5a8f6-default-rtdb.europe-west1.firebasedatabase.app');
 
 $database = $factory->createDatabase();
