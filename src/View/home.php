66ae7856c1bedd65c8dd7bb4be3a5a0f8618324e
Excellence KAWEJ <19ka201@esisalama.org>
2022-04-23 19:55:03 +0200

    modify the home page

--- src/View/home.php
+++ src/View/home.php
@@ -7,6 +7,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <title>MYSPACE-Accueil</title>
@@ -37,38 +38,59 @@
         </nav>
     </header>
     <div class="container">
-        <div class="col mb-3 bg-white">
-            <p class="h4">Vendredi, 22 avril 2022</p>
-            <p class="h3">Bienvenue <?php echo "'Excellence KAWEJ'"; ?> sur MySpace</p>
-        </div>
-        <?php
-        for ($i = 0; $i < 10; $i++) {
-            ?>
-                <div class="col bg-white mb-2">
-                    <div class="row">
-                        <div class="col-1">
-                            <img src="" alt="photo de profil">
-                        </div>
-                        <div class="col">
-                            <p class="h3"><?php echo "Excellence KAWEJ"?></p>
-                        </div>
-                    </div>
-                    <div class="col offset-1">
-                        <p class="h5"><?php echo "Data Scientist"?></p>
-                        <p>Je suis devenue analyste de données ce qui me permet de pouvoir traiter, analyser ou autrement de faire parler les données</p>
-                    </div>
-                    <div class="row">
-                        <div class="col">
-                            <p class="text-center">Likez</p>
-                        </div>
-                        <div class="col">
-                            <p class="text-center">Commentez</p>
-                        </div>
-                    </div>
+        <div class="row">
+            <div class="col-8">
+                <div class="col mb-3 bg-white">
+                    <p class="h4">Vendredi, 22 avril 2022</p>
+                    <p class="h3">Bienvenue <?php echo "Excellence KAWEJ"; ?> sur MySpace</p>
                 </div>
-            <?php
-        }
-        ?>
+                <?php
+                for ($i = 0; $i < 10; $i++) {
+                    ?>
+                        <div class="col bg-white mb-2">
+                            <div class="row">
+                                <div class="col-1 ">
+                                    <img src="" alt="photo de profil" class="">
+                                </div>
+                                <div class="col">
+                                    <p class="h3"><?php echo "Excellence KAWEJ"?></p>
+                                </div>
+                            </div>
+                            <div class="col offset-1">
+                                <p class="h5"><?php echo "Data Scientist"?></p>
+                                <p>Je suis devenue analyste de données ce qui me permet de pouvoir traiter, analyser ou autrement de faire parler les données</p>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="row">
+                                        <div class="col d-flex justify-content-end">
+                                            <i class="fa-solid fa-heart"></i>
+                                        </div>
+                                        <div class="col">
+                                            <p class="text-start">Likez</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="row">
+                                        <div class="col d-flex justify-content-end">
+                                            <i class="fa-solid fa-comment"></i>
+                                        </div>
+                                        <div class="col">
+                                            <p class="text-start">Commentez</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    <?php
+                }
+                ?>
+            </div>
+            <div class="col bg-secondary">
+
+            </div>
+        </div>
     </div>
 </body>
 </html>
