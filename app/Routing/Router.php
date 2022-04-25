d3e4e5d1cc5829cb544fef0f602ec6c8b5735d4b
Josh-Muleshi <jmuleshi2@gmail.com>
2022-04-25 09:56:17 +0200

    fixes #5

--- app/Routing/Router.php
+++ app/Routing/Router.php
@@ -1,23 +1,2 @@
 <?php
 
-    require '../vendor/autoload.php';
-
-    $uri = $_SERVER['REQUEST_URI'];
-    $router = new AltoRouter();
-
-    $router->map('GET', '/', function(){
-        echo "salut";
-    });
-    $router->map('GET', '/home', function(){
-        echo 'home';
-    },'home');
-
-    $match = $router->match();
-
-    // call closure or throw 404 status
-    if( is_array($match) && is_callable( $match['target'] ) ) {
-        call_user_func_array( $match['target'], $match['params'] ); 
-    } else {
-        // no route was matched
-        header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
-    }
--- public/index.php
+++ public/index.php
@@ -9,4 +9,24 @@
      * @author Josh Muleshi <jmuleshi2@gmail.com>
      */
 
-     echo "MySpace";
+    require '../vendor/autoload.php';
+
+    $uri = $_SERVER['REQUEST_URI'];
+    $router = new AltoRouter();
+
+    $router->map('GET', '/', function(){
+        echo "salut";
+    });
+    $router->map('GET', '/home', function(){
+        echo 'home';
+    },'home');
+
+    $match = $router->match();
+
+    // call closure or throw 404 status
+    if( is_array($match) && is_callable( $match['target'] ) ) {
+        call_user_func_array( $match['target'], $match['params'] );
+    } else {
+        // no route was matched
+        header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+    }
