dd645ce6aa115c0e91a878d6eb9af97a28bfc6df
Josh-Muleshi <jmuleshi2@gmail.com>
2022-05-23 22:01:09 +0200

    done some changes

--- public/index.php
+++ public/index.php
@@ -1,32 +1,27 @@
 <?php
-
     /**
      * @package myspace
      * 
      * MySpace est une application de gestion des profils 
      * specialement pour les etudiants d'esis test
      * 
-     * @author Josh Muleshi <jmuleshi2@gmail.com>
-     */
+     * @author Josh Muleshi <jmuleshi2@gmail.com>*/
+
+    use Route\Router;
+    use Route\RouterException;
 
     require '../vendor/autoload.php';
 
-    $uri = $_SERVER['REQUEST_URI'];
-    $router = new AltoRouter();
+    $router = new Router();
 
-    $router->map('GET', '/', function(){
-        require '../src/View/home.php';
-    });
-    $router->map('GET', '/home', function(){
-        echo 'home';
-    },'home');
+    $router->get('/', function (){ require '../views/home.php'; });
+    $router->get('/posts', function (){ echo 'Les articles !'; });
+    $router->get('/posts/:id', function ($id){ echo 'L\'articles ' . $id; });
+    $router->post('/posts/:id', function ($id){ echo 'L\'articles ' . $id; });
 
-    $match = $router->match();
-
-    // call closure or throw 404 status
-    if( is_array($match) && is_callable( $match['target'] ) ) {
-        call_user_func_array( $match['target'], $match['params'] );
-    } else {
-        // no route was matched
-        header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+    try {
+        $router->run();
+    } catch (RouterException $e) {
+        echo $e->getMessage();
     }
+
